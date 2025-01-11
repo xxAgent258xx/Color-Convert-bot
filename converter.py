121312fd662501be5862006ed059d35855a87cc9
PotRaptor258 <98768127+PotRaptor258@users.noreply.github.com>
2025-01-11 19:18:10 +0300

    v2.4.001
    Again...

--- converter.py
+++ converter.py
@@ -657,24 +657,6 @@ async def inline_mode(inline_query: InlineQuery):
             await bot.send_message(ADMIN_ID,
                                    f'{'@' + inline_query.from_user.username if inline_query.from_user.username else 'tg://openmessage?user_id=' + str(inline_query.from_user.id)}\n{e}')
 
-            await bot.answer_inline_query(query_id,
-                                          [InlineQueryResultArticle(
-                                                  id=str(int(query_id) + 3),
-                                                  type=InlineQueryResultType.ARTICLE,
-                                                  title=f'Без фото',
-                                                  input_message_content=InputTextMessageContent(
-                                                      message_text=f'✨Pantone: {year_pantone}\n'
-                                                                   f'✨HEX: #{year_hex}\n'
-                                                                   f'✨RGB: {year_rgb}\n'
-                                                                   f'✨CMYK: {year_cmyk}\n'
-                                                                   f'✨{ans_url}{year_hex}'),
-                                                  hide_url=True,
-                                                  description=f'Pantone: {year_pantone}\n'
-                                                              f'HEX: #{year_hex}\n'
-                                                              f'RGB: {year_rgb}\n'
-                                                              f'CMYK: {year_cmyk}'
-                                              )])
-
 
 @dp.message()
 async def send_echo(message: Message):
