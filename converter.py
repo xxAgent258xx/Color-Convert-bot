7591739d214326e69095ad274fe4485dc89fa6b2
PotRaptor258 <98768127+PotRaptor258@users.noreply.github.com>
2025-01-15 22:35:46 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from json import loads
+# from json import loads
 
 import aiohttp
 from aiogram import Bot, Dispatcher, F
@@ -463,7 +463,6 @@ async def inline_mode(inline_query: InlineQuery):
     check = True
     scheme = []
     query_id = ''
-    response = {}
     try:
         query = str(inline_query)[str(inline_query).index("query='") + 7:str(inline_query).index("offset='") - 2]
         query_id: str = inline_query.id
@@ -481,12 +480,14 @@ async def inline_mode(inline_query: InlineQuery):
                     async with aiohttp.ClientSession() as session:
                         async with session.get(f'{api_url}rgb=rgb({r},{g},{b})') as response_:
                             response = await response_.json()
+
                 elif scheme[0].lower() == 'hex':
                     hex = scheme[1]
                     if len(hex) == 6 or len(hex) == 3:
                         async with aiohttp.ClientSession() as session:
                             async with session.get(f'{api_url}hex={hex}') as response_:
                                 response = await response_.json()
+
                 elif scheme[0].lower() == 'cmyk':
                     c, m, y, k = scheme[1], scheme[2], scheme[3], scheme[4]
                     if 0 <= int(c) <= 100 and 0 <= int(m) <= 100 and 0 <= int(y) <= 100 and 0 <= int(k) <= 100:
@@ -494,52 +495,9 @@ async def inline_mode(inline_query: InlineQuery):
                             async with session.get(f'{api_url}cmyk={c},{m},{y},{k}') as response_:
                                 response = await response_.json()
 
-                elif scheme[0].lower() == 'year':
-                    await bot.answer_inline_query(query_id,
-                                                  [InlineQueryResultPhoto(
-                                                      type=InlineQueryResultType.PHOTO,
-                                                      id=str(int(query_id) + 1),
-                                                      photo_url=f'{ans_pic}{year_hex}/{year_hex}.jpeg',
-                                                      thumbnail_url=f'{ans_pic}{year_hex}/{year_hex}.jpeg',
-                                                      caption=f'✨Pantone: {year_pantone}\n'
-                                                              f'✨HEX: #{year_hex}\n'
-                                                              f'✨RGB: {year_rgb}\n'
-                                                              f'✨CMYK: {year_cmyk}\n'
-                                                              f'✨{ans_url}{year_hex}',
-                                                      title=f'С фото',
-                                                      description=f'Pantone: {year_pantone}\n'
-                                                                  f'HEX: #{year_hex}\n'
-                                                                  f'RGB: {year_rgb}\n'
-                                                                  f'CMYK: {year_cmyk}'
-                                                  ),
-                                                      InlineQueryResultArticle(
-                                                          id=str(int(query_id) + 2),
-                                                          type=InlineQueryResultType.ARTICLE,
-                                                          title=f'Без фото',
-                                                          input_message_content=InputTextMessageContent(
-                                                              message_text=f'✨Pantone: {year_pantone}\n'
-                                                                           f'✨HEX: #{year_hex}\n'
-                                                                           f'✨RGB: {year_rgb}\n'
-                                                                           f'✨CMYK: {year_cmyk}\n'
-                                                                           f'✨{ans_url}{year_hex}'),
-                                                          hide_url=True,
-                                                          description=f'Pantone: {year_pantone}\n'
-                                                                      f'HEX: #{year_hex}\n'
-                                                                      f'RGB: {year_rgb}\n'
-                                                                      f'CMYK: {year_cmyk}'
-                                                      )])
                 else:
                     check = False
-        except ValueError:
-            pass
-        except IndexError:
-            pass
-        except TelegramBadRequest:
-            pass
-        except Exception as e:
-            await bot.send_message(ADMIN_ID,
-                                   f'{'@' + inline_query.from_user.username if inline_query.from_user.username else 'tg://openmessage?user_id=' + str(inline_query.from_user.id)}\n{e}')
-        try:
+
             if check and scheme[0].lower() != 'year':
                 response_hex = str(response['hex']['clean']).upper()
                 response_r = 0 if response['rgb']['r'] is None else response['rgb']['r']
@@ -580,8 +538,47 @@ async def inline_mode(inline_query: InlineQuery):
                                                                   f'RGB: {response_r} {response_g} {response_b}\n'
                                                                   f'CMYK: {response_c} {response_m} {response_y} {response_k}',
                                                   )])
-        except ValueError:
+            elif scheme[0].lower() == 'year':
+                await bot.answer_inline_query(query_id,
+                                              [InlineQueryResultPhoto(
+                                                  type=InlineQueryResultType.PHOTO,
+                                                  id=str(int(query_id) + 1),
+                                                  photo_url=f'{ans_pic}{year_hex}/{year_hex}.jpeg',
+                                                  thumbnail_url=f'{ans_pic}{year_hex}/{year_hex}.jpeg',
+                                                  caption=f'✨Pantone: {year_pantone}\n'
+                                                          f'✨HEX: #{year_hex}\n'
+                                                          f'✨RGB: {year_rgb}\n'
+                                                          f'✨CMYK: {year_cmyk}\n'
+                                                          f'✨{ans_url}{year_hex}',
+                                                  title=f'С фото',
+                                                  description=f'Pantone: {year_pantone}\n'
+                                                              f'HEX: #{year_hex}\n'
+                                                              f'RGB: {year_rgb}\n'
+                                                              f'CMYK: {year_cmyk}'
+                                              ),
+                                                  InlineQueryResultArticle(
+                                                      id=str(int(query_id) + 2),
+                                                      type=InlineQueryResultType.ARTICLE,
+                                                      title=f'Без фото',
+                                                      input_message_content=InputTextMessageContent(
+                                                          message_text=f'✨Pantone: {year_pantone}\n'
+                                                                       f'✨HEX: #{year_hex}\n'
+                                                                       f'✨RGB: {year_rgb}\n'
+                                                                       f'✨CMYK: {year_cmyk}\n'
+                                                                       f'✨{ans_url}{year_hex}'),
+                                                      hide_url=True,
+                                                      description=f'Pantone: {year_pantone}\n'
+                                                                  f'HEX: #{year_hex}\n'
+                                                                  f'RGB: {year_rgb}\n'
+                                                                  f'CMYK: {year_cmyk}'
+                                                  )])
+
+        except UnboundLocalError:
             pass
+        # except ValueError:
+        #     pass
+        # except IndexError:
+        #     pass
         except Exception as e:
             await bot.send_message(ADMIN_ID,
                                    f'{'@' + inline_query.from_user.username if inline_query.from_user.username else 'tg://openmessage?user_id=' + str(inline_query.from_user.id)}\n{e}')
